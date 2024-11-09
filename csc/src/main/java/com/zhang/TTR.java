e252bf50b6110b07f01d600cd93a3bda4d485e2d
Zyijie <347588831@qq.com>
2024-11-09 18:46:13 +0800

    第三次测试

---
+++ csc/src/main/java/com/zhang/TTR.java
@@ -0,0 +1,13 @@
+package com.zhang;
+
+/**
+ * @Version :1.0
+ * @Author :张義杰
+ * @File :TTR.py
+ * @Time :2024/11/9 18:45
+ */
+public class TTR {
+    public static void main(String[] args) {
+        System.out.println("niao2");
+    }
+}
