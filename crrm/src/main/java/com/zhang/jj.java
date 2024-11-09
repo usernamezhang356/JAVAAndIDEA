6c4850a7e25789a1e0561befa5150ba90982e7ec
Zyijie <347588831@qq.com>
2024-11-09 18:09:40 +0800

    我试着做一下测试，看看能不能提交到本地

---
+++ crrm/src/main/java/com/zhang/jj.java
@@ -0,0 +1,13 @@
+package com.zhang;
+
+/**
+ * @Version :1.0
+ * @Author :张義杰
+ * @File :jj.py
+ * @Time :2024/11/9 18:05
+ */
+public class jj {
+    public static void main(String[] args) {
+        System.out.println("Hi jj");
+    }
+}
